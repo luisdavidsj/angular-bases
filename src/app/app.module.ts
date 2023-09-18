fd86b5548f38828b163a7770dda24cc659ec4d99
Luis David Saenz Jurado <luisdavidspider@gmail.com>
2023-09-17 23:03:04 -0600

    Fin de seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,19 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
-    CounterModule
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
